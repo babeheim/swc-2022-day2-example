6d1abb0b040ae2f29073ab0b3e84e4827d606b54
Bret Beheim <beheim@gmail.com>
2022-11-22 16:28:06 +0100

    changes: - streamlined 2_analysis.R - cleaned up figure names - deleted for manuscript folder

--- 2_analysis.R
+++ 2_analysis.R
@@ -5,17 +5,9 @@ load("data_clean.RData")
 
 nrow(dat) # 16 observations
 
-# summary(dat$Sex)
-
-# summary(dat$Age_Part)
 # mean is 60.00 years old
 
-# m0 <- lm(ln_cac_plus1 ~ Age_Part, data = dat)
 m1 <- lm(ln_cac_plus1 ~ Age_Part + Sex, data = dat)
-
-# png("analysisplot.png", res = 300, height = 5, width = 5, units = "in")
-# plot(dat$Age_Part, dat$ln_cac_plus1, pch = ifelse(dat$Sex == 1, 1, 20))
-# dev.off()
 
 png("analysisplot_new.png", res = 300, height = 5, width = 5, units = "in")
 plot(dat$Age_Part, dat$ln_cac_plus1, pch = ifelse(dat$Sex == 1, 1, 20))
@@ -23,6 +15,6 @@ curve(coef(m1)[1] + coef(m1)[2] * x, add = TRUE, col = "gray")
 curve(coef(m1)[1] + coef(m1)[2] * x + coef(m1)[3], add = TRUE)
 dev.off()
 
-# png("boxplot.png", res = 300, height = 5, width = 5, units = "in")
-# boxplot(dat$ln_cac_plus1 ~ dat$male)
-# dev.off()
+png("boxplot.png", res = 300, height = 5, width = 5, units = "in")
+boxplot(dat$ln_cac_plus1 ~ dat$male)
+dev.off()
