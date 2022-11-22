d99cd026480c3e1e300c17063a14de4e307e5382
Bret Beheim <beheim@gmail.com>
2022-11-22 15:56:28 +0100

    changed data processing to use the correct source file

--- analysis clean.R
+++ analysis clean.R
@@ -1,4 +1,24 @@
 
+setwd("/Users/bretbeheim/Dropbox/projects/2021/swc-spring-school/cardio-multisite-raw/site 1")
+
+library(readxl)
+
+# dat <- read_xlsx("Data_Final_092315.xlsx")
+# dat <- read_xlsx("Data_Final_092315 copy.xlsx")
+dat <- read_xlsx("Data_Final_092315 BAB.xlsx") # USE THIS ONE
+
+dat$CoronaryCA <- as.numeric(dat$CoronaryCA)
+dat$Age_Part <- as.numeric(dat$Age_Part)
+dat$Sex <- as.factor(dat$Sex)
+
+dat$ln_cac_plus1 <- log(dat$CoronaryCA + 1)
+
+dat <- filter(dat, !is.na(CoronaryCA) & Demo != 1 & Sex != 3)
+
+dat$male <- as.numeric(dat$Sex == 1)
+
+save(dat, file = "data_clean.RData")
+
 rm(list = ls())
 
 load("data_clean.RData")
